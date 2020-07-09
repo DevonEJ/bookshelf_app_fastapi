7c7b91e0d0589cb81cf67d73f7c77ffec81a7d36
Devon Edwards Joseph <devonedwardsjoseph@devons-mbp.home>
2020-07-09 21:15:16 +0100

    add email validation to user model

--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from fastapi import Query
 import enum
 
 
@@ -10,8 +11,6 @@ class Role(str, enum.Enum):
 class User(BaseModel):
     id: str
     name: str
-    email: str
+    email: str = Query(..., regex='[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+', title="Validate Email")
     role: Role
     bookshelf_id: str
-
-
