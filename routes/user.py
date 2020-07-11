a3ffeba7bead098ccfdb93abf372b4bbbb54efa4
Devon Edwards Joseph <devonedwardsjoseph@devons-mbp.home>
2020-07-11 19:52:28 +0100

    add 201 status code to create user endpoint

--- routes/user.py
+++ routes/user.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from models.user import User
+from starlette.status import HTTP_201_CREATED
 
 app = FastAPI()
 
 
-@app.post("/user")
+@app.post("/user", status_code=HTTP_201_CREATED)
 async def create_user(user: User):
     print(user)
 
