b7415b1b63de809ddea9ee8c792f06e200ffb035
Devon Edwards Joseph <devonedwardsjoseph@devons-mbp.home>
2020-07-11 22:40:49 +0100

    adding jwt token endpoint skeleton

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 
 from .routes.v1 import author, book, home, user
 
 app = FastAPI()
+
+oauth_schema = OAuth2PasswordBearer(tokenUrl="/token")
+
+
+@app.post("/token")
+async def login_for_access_token(oauth_form: OAuth2PasswordRequestForm = Depends()):
+    pass
+
 
 app.include_router(author.router, prefix="/v1")
 app.include_router(book.router, prefix="/v1")
