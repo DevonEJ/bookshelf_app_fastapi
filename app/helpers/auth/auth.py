f8825563148ad534c61797cb0210f7b67f5855a1
Devon Edwards Joseph <devonedwardsjoseph@devons-mbp.home>
2020-07-12 17:18:20 +0100

    add temp auth function skeleton, and dotenv loading

--- app/helpers/auth/auth.py
+++ app/helpers/auth/auth.py
@@ -1,8 +1,15 @@
-
+import os
 from passlib.context import CryptContext
+from dotenv import find_dotenv, load_dotenv
 
 from app.models.v1.jwt_user import JWTUser
 
+load_dotenv(find_dotenv())
+
+# Loaded from .env file via settings.py
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
+JWT_EXPIRE_AFTER_MINUTES = os.getenv("JWT_EXPIRE_AFTER_MINUTES")
 
 password_context = CryptContext(schemes=["bcrypt"])
 
@@ -23,10 +30,17 @@ fake_jwt_user = {"username": "test", "password": "", "disabled": False, "role": 
 fake_jwt_user = JWTUser(**fake_jwt_user)
 
 
-print(encrypt_password("password"))
-
-
 # Creation of JWT
-def authenticate_user(username: str, password: str):
+def authenticate_user(username: str, password: str) -> bool:
+    # If user is in the database
+    if fake_jwt_user.username == username:
+        if not fake_jwt_user.disabled:
+            if check_encrypted_password(password, encrypt_password(fake_jwt_user.password)):
+                return True
+
+    return False
+
+
+def create_jwt_token(username: str):
     pass
 
