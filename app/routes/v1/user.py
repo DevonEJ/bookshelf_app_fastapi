e19ba846eae4c05c230a0413ab4c6e8085554e59
Devon Edwards Joseph <devonedwardsjoseph@devons-mbp.home>
2020-07-18 18:05:35 +0100

    adding jwt check to create_user

--- app/routes/v1/user.py
+++ app/routes/v1/user.py
@@ -1,13 +1,14 @@
-from fastapi import APIRouter, File
+from fastapi import APIRouter, File, Header, Depends
 from app.models.v1.user import User
 from starlette.status import HTTP_201_CREATED
+from app.helpers.auth.auth import verify_jwt_token
 
 router = APIRouter()
 
 
 @router.post("/user", status_code=HTTP_201_CREATED)
-async def create_user(user: User):
-    print(user)
+async def create_user(user: User, x_custom: str = Header("default"), jwt: bool = Depends(verify_jwt_token) ):
+    return{"request body": user, "request custom header": x_custom}
 
 
 @router.post("/user/image")
